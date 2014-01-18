154ffb6823d88671f6ecf4021f00199e8ee751a3
Cougar Tech <cougartech@HS148T1600-03.hflcampus.monroe.edu>
2014-01-18 10:30:29 -0500

    Constructed the Jaguars

--- src/cougartech/aerialassist/modules/Shooter.java
+++ src/cougartech/aerialassist/modules/Shooter.java
@@ -1,10 +1,23 @@
 package cougartech.aerialassist.modules;
 
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
 public class Shooter
 {
-    
-    public Shooter(int value)
+    CANJaguar m1;
+    CANJaguar m2;
+            
+    public Shooter(int m1Port, int m2Port)
     {
-        
+        try
+        {    
+            m1 = new CANJaguar(m1Port, CANJaguar.ControlMode.kPosition);
+            m2 = new CANJaguar(m2Port, CANJaguar.ControlMode.kPosition);
+        }
+        catch(CANTimeoutException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
